79d7098dbeff8b64d249f9b30163641f43adf164
jean <jin.ahn.1027@gmail.com>
2021-02-09 15:32:53 -0500

    commit tree 8fb618319975e49d02eed7812104b735701f2f62

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+    attr_accessor :school_name
+    def initialize(school_name)
+        @school_name=school_name  
+        @roster={}
+    end
+    
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        #if @roster[grade]
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        list={}
+        @roster.each do |key, value|
+            list[key]=value.sort
+        end
+        list
+    end
+
+end
+ 
